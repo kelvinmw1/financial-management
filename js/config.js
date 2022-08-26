37709c3f300f280c2da6a55f5e4dea999e41795c
kelvinmw1 <qklint@gmail.com>
2022-08-26 13:05:55 +0300

    styling the system

---
+++ js/config.js
@@ -0,0 +1,15 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyA-1Vok1gE4doInx_HJwoiW2R13NNpMtpY",
+    authDomain: "my-twitter-12cd4.firebaseapp.com",
+    projectId: "my-twitter-12cd4",
+    storageBucket: "my-twitter-12cd4.appspot.com",
+    messagingSenderId: "317646262590",
+    appId: "1:317646262590:web:7f7d4c2d6e61856b1e5c6f",
+    measurementId: "G-69BZMX0K9E"
+  };
+
+  //initializing firebase
+
+  firebase.initializeApp(firebaseConfig);
