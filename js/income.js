a609c7e0a614fcd62b072453c9950c2b802f9076
kelvinmw1 <qklint@gmail.com>
2022-09-08 00:41:06 +0300

    ui/ux changes and income logic

---
+++ js/income.js
@@ -0,0 +1,46 @@
+document.getElementById("Submit").onclick = function(){
+    let incomeType = document.getElementById("incomeType").value;
+    let paymentMethod = document.getElementById("paymentMethod").value;
+    let amount = document.getElementById("amount").value;
+    let receivedBy = document.getElementById("receivedBy").value;
+    let timeStamp = new Date();
+
+    let addIncome = firebase.firestore().collection("income").doc();
+    addIncome.set({
+
+        docId:addIncome.id,
+        timeStamp:timeStamp,
+        incomeType:incomeType,
+       paymentMethod:paymentMethod,
+        amount:amount,
+        receivedBy:receivedBy
+    }).then(()=>{
+        window.location.reload();
+    })
+}
+// Read Income
+
+firebase.firestore().collection("income").get().then((querySnapshot)=>{
+    let content = '';
+    querySnapshot.forEach((doc)=>{
+        let incomeType = doc.data().incomeType;
+        let paymentMethod = doc.data().paymentMethod;
+        let amount = doc.data().amount;
+        let receivedBy = doc.data().receivedBy;
+        let timeStamp = doc.data().timeStamp;
+        let docId = doc.data().docId;
+
+
+        content+= '<tr>' 
+
+            content+= '<td>'+incomeType+'</td>'
+            content+= '<td>'+paymentMethod+'</td>'
+            content+= '<td> KES '+amount+'</td>'
+            content+= '<td>'+receivedBy+'</td>'
+            content+= '<td><button class="btn btn-success">Edit</button></td>'
+            content+= '<td><a href="" class="btn btn-danger">Delete</a></td>'
+
+        content+= '</tr>'
+    })
+    $("#incomelist").append(content);
+})
